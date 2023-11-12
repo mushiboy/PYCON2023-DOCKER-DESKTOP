01c5391d1b607ea0d17b75b4d1036a0c9e64d864
Mugesh <mushi025@gmail.com>
2023-11-12 10:11:41 +0000

    Basic

--- src/screen.py
+++ src/screen.py
@@ -1,6 +1,16 @@
 import PySimpleGUI as sg
 
-window = sg.Window("PyCon Ireland", [[]], size=(600, 600))
+
+layout = [[sg.Column(
+    [[
+        sg.Text("Sample")
+    ]], size=(300, 600)
+),
+    sg.Table(values=[[1, 2, 3]], headings=[
+        "a", "b", "c"], size=(300, 300), expand_x=True), ]]
+
+window = sg.Window("PyCon Ireland", layout, size=(600, 600))
+
 
 while True:
     event, value = window.read()
